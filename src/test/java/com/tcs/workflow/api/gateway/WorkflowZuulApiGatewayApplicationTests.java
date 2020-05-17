e102efa67c0fe67355cc67ccc790d34da8d54998
HimanshuPandey <himanshugithub6201@gmail.com>
2020-05-17 20:06:07 +0530

    1t commit

---
+++ src/test/java/com/tcs/workflow/api/gateway/WorkflowZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.workflow.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
